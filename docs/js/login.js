8c2e0cdec7905333f8669f6e8fcccdee2371d528
Fernando10104 <carinafaviola85@gmail.com>
2025-08-11 22:24:39 -0300

    Actualización de archivos y nuevos módulos 2 config cambiado

--- docs/js/login.js
+++ docs/js/login.js
@@ -46,7 +46,7 @@ document.getElementById("login-form").addEventListener("submit", async function 
     const formData = new URLSearchParams();
     formData.append("username", username);
     formData.append("password", password); 
-    const URL_api = "http://localhost:8000"; // Cambia esto por tu URL base
+    const URL_api = "https://word.puntodigitalpy.online"; // Cambia esto por tu URL base
 
     try {
         const response = await fetch(`${URL_api}/login`, {
