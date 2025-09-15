15668426df2dbf05016321aa06b03fd6bef8dd27
Fernando10104 <carinafaviola85@gmail.com>
2025-09-15 11:30:02 -0300

    MODIFICACION DE OPRACION ARREGLO DE SELECT DE NUEVO Y RENOVACION CONFIG

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "http://localhost:8000"; // Cambia esto por tu URL base
-//export const API_BASE_URL = "https://word.puntodigitalpy.online";
+//export const API_BASE_URL = "http://localhost:8000"; // Cambia esto por tu URL base
+export const API_BASE_URL = "https://word.puntodigitalpy.online";
 
