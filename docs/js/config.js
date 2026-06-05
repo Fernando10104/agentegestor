0fd7b49dd11f60d651d1ecf2b9429c0bbff28546
Alan10104XD <alanenciso05@gmail.com>
2026-06-05 09:19:06 -0300

    Fix: urls api

--- docs/js/config.js
+++ docs/js/config.js
@@ -1,3 +1,3 @@
 //export const API_BASE_URL = "http://localhost:8000"; // Cambia esto por tu URL base
-export const API_BASE_URL = "/";
+export const API_BASE_URL = "https://api.evolucionpy.com.py";
 
